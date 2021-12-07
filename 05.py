9ecbade7da58040b9f747e79be20bdcb838e3357
ByteEater <km@localhost.localdomain>
2021-12-07 06:27:58 +0100

    Improve solution of Day 5

--- 05.py
+++ 05.py
@@ -5,9 +5,8 @@ y_max = 0
 for l in I.input:
 	*t, = map(int, l.replace(' -> ', ',').split(','))
 	coords += [t]
-	x1, y1, x2, y2 = t
-	x_max = max(x_max, x1, x2)
-	y_max = max(y_max, y1, y2)
+	x_max = max(x_max, t[0], t[2])
+	y_max = max(y_max, t[1], t[3])
 f_diag, f_no_diag = (
 	[[0] * (y_max + 1) for _ in range(x_max + 1)] for _ in range(2))
 c_diag = 0
